115f159ef77232ab221eac4241033684c8ed69ec
Prem Patel <poojanpatel2121@gmail.com>
2025-06-09 19:57:55 +0530

    error in redis

--- backend/src/bullMq/redis.ts
+++ backend/src/bullMq/redis.ts
@@ -1,3 +1,6 @@
 import { Redis } from "ioredis";
 
-export const connection = new Redis(process.env.UPSTASH_REDIS_URL as string);
+export const connection = new Redis(process.env.UPSTASH_REDIS_URL as string, {
+  maxRetriesPerRequest: null,
+  tls: {},
+});
