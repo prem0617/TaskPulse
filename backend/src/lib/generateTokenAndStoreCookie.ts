98fee46edb174a2b09aaac6ffc987f190b3fb003
Prem Patel <poojanpatel2121@gmail.com>
2025-06-10 17:15:34 +0530

    solving the cookies error

--- backend/src/lib/generateTokenAndStoreCookie.ts
+++ backend/src/lib/generateTokenAndStoreCookie.ts
@@ -14,8 +14,8 @@ function generateTokenAndStoreCookie(data: IGenerateToken) {
 
   res.cookie("tmtoken", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
